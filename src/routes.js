bbb3bc6d061d2b246d869b2806a66be4fdc22300
jkhlr <13289385+jkhlr@users.noreply.github.com>
2019-11-15 18:00:58 -0800

    remove rateLimit and sizeValidator

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,21 @@
 const router = require('express').Router();
-const rateLimit = require("express-rate-limit");
+//const rateLimit = require("express-rate-limit");
 
 const model = require('./model');
 const validators = require('./validators');
 
 // list of all validators to be in place
 router.use(validators.removeNativeKeys);
-router.use(validators.sizeValidator);
+//router.use(validators.sizeValidator);
 router.use(validators.keysValidator);
 router.use(validators.extractParams);
 router.use(validators.validateParams);
 
-router.post('/*', rateLimit({
-	windowMs: 60 * 60 * 1000, // In 60 minutes window
-	max: 100 // only 100 POST requests are allowed
-}), model.xpost);
+// router.post('/*', rateLimit({
+// 	windowMs: 60 * 60 * 1000, // In 60 minutes window
+// 	max: 100 // only 100 POST requests are allowed
+// }), model.xpost);
+router.post('/*', model.xpost);
 router.get('/*', model.xget);
 router.put('/*', model.xput);
 router.delete('/*', model.xdelete);
